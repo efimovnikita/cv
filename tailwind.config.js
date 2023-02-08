ddb8598cb5378c6fa5db505dfe68a85817a7751b
maskedball <maskedballmail@gmail.com>
2023-02-08 13:38:13 +0300

    add new data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['JetBrains Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        "background-color": "#333333",
+        "h1-color": "#99FFCC",
+        "h2-color": "#15BCBC",
+        "h3-color": "#2299CC",
+        "h4-color": "#52528E",
+        "text-color": "#C8C8C8",
+        "link-color": "#F45C5C",
+      },
+    },
   },
   plugins: [],
-}
+};
